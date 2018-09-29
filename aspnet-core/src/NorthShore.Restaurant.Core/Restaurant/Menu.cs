b0641dffd23b20ef9203589e738c7531103a89af
sinan nar <sinan.nar@gmail.com>
2018-09-29 22:37:38 +1200

    menu entity has been updated

--- aspnet-core/src/NorthShore.Restaurant.Core/Restaurant/Menu.cs
+++ aspnet-core/src/NorthShore.Restaurant.Core/Restaurant/Menu.cs
@@ -10,6 +10,9 @@ namespace NorthShore.Restaurant.Restaurant
     {
         public string Name { get; set; }
         public decimal DiscountRate { get; set; }
+        public decimal TotalCalorie{get;set;}
+        public decimal TotalPrice{get;set;}
+        public decimal DiscountedPrice{get;set;}
         public IEnumerable<FoodMenuMapping> FoodMappings { get; set; }
     }
 }
---
+++ aspnet-core/src/NorthShore.Restaurant.EntityFrameworkCore/Migrations/20180929103704_AddingDescriptiveFieldsToMenu.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NorthShore.Restaurant.Migrations
+{
+    public partial class AddingDescriptiveFieldsToMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountedPrice",
+                table: "Menus",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalCalorie",
+                table: "Menus",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "Menus",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountedPrice",
+                table: "Menus");
+
+            migrationBuilder.DropColumn(
+                name: "TotalCalorie",
+                table: "Menus");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Menus");
+        }
+    }
+}
